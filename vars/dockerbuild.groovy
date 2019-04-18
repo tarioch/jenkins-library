4e66a60a2f47882a3fe1957939b2785376ba38c3
Patrick Ruckstuhl <patrick@ch.tario.org>
2019-04-18 23:32:18 +0200

    initial version of dockerbuild step

---
+++ vars/dockerbuild.groovy
@@ -0,0 +1,58 @@
+class DockerbuildParams {
+    String registry = ''
+    String repository
+    String tag = ''
+}
+
+def call(Map params) {
+    doCall(params as DockerbuildParams)
+}
+
+def doCall(DockerbuildParams params) {
+    def label = "kaniko-${UUID.randomUUID().toString()}"
+
+    podTemplate(name: 'kaniko', label: label, yaml: """
+    kind: Pod
+    metadata:
+        name: kaniko
+    spec:
+    containers:
+    - name: kaniko
+        image: gcr.io/kaniko-project/executor:latest
+        imagePullPolicy: Always
+        command:
+        - /busybox/cat
+        tty: true
+    """
+    ) {
+        node(label) {
+            def commitHash
+            def registry = params.registry
+            def repository = params.repository
+            def tag = params.tag
+            stage('Checkout') {
+                 def scmVars = checkout scm
+                 commitHash = scmVars.GIT_COMMIT
+            }
+            stage('Build') {
+                if (registry == '') {
+                    registry = 'registry.tario.org'
+                }
+                if (tag == '') {
+                    tag = "${BRANCH_NAME}-${commitHash}"
+                }
+                def image = "${registry}/${repository}:${tag}"
+                echo image
+                container(name: 'kaniko', shell: '/busybox/sh') {
+                    withEnv(['PATH+EXTRA=/busybox:/kaniko']) {
+                        ansiColor('xterm') {
+                            sh """#!/busybox/sh
+                            /kaniko/executor -f `pwd`/Dockerfile -c `pwd` --cache=true --destination=${image}
+                            """
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
