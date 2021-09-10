9e671cc35364799d6980b0e9afc0aa30e82b948b
Patrick Ruckstuhl <patrick@ch.tario.org>
2021-09-10 21:53:08 +0000

    upgrade to latest kaniko

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -18,7 +18,7 @@ metadata:
 spec:
   containers:
   - name: kaniko
-    image: gcr.io/kaniko-project/executor:debug-v0.16.0
+    image: gcr.io/kaniko-project/executor:v1.6.0
     imagePullPolicy: Always
     command:
     - /busybox/cat
